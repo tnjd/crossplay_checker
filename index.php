583e405849aed5ac57755b818b7487b60438e961
expectocode <expectocode@gmail.com>
2026-05-23 13:07:04 +0100

    minor html fixes

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <meta charset="utf-8">
 <meta name="viewport"
       content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
-<link rel="stylesheet" href="main.css"></link>
+<link rel="stylesheet" href="main.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Manrope:wght@200..800&family=Noto+Emoji:wght@300..700&display=swap" rel="stylesheet">
@@ -45,7 +45,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     Crossplay Word Checker
     </h1>
 
-    <form method="POST" action="">
+    <form method="POST">
         <input type="text" name="word" value="<?= htmlspecialchars($searched) ?>"><button type="submit">🔍</button>
     </form>
 
